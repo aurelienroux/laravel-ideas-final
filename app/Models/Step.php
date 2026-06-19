1470f15afe26fa85c80c3882f8d32123a1998f08
Aurelien Roux <aureroux13@gmail.com>
2026-06-19 12:29:51 +0200

    step model

---
+++ app/Models/Step.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\StepFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Step extends Model
+{
+    /** @use HasFactory<StepFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'description',
+        'completed',
+    ];
+
+    protected $attributes = [
+        'completed' => false,
+    ];
+
+    public function idea(): BelongsTo
+    {
+        return $this->belongsTo(Idea::class);
+    }
+}
